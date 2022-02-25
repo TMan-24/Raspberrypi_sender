f09a015f698345fc182661a1327a89f921d23928
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-02-25 11:13:08 -0600

    new file

---
+++ linetest4sensors.py
@@ -0,0 +1,87 @@
+from pickle import TRUE
+from time import sleep
+import RPi.GPIO as gpio
+
+in1 = 25
+in2 = 24
+in3 = 23
+in4 = 22
+RightMiddlelinesensor = 4
+LeftMiddlelinesensor = 27
+Rightlinesensor = 3 #not real
+Leftlinesensor = 9 #not real
+enA = 12
+enB = 13
+
+gpio.setmode(gpio.BCM)
+gpio.setup(RightMiddlelinesensor, gpio.IN)
+gpio.setup(LeftMiddlelinesensor, gpio.IN)
+gpio.setup(Rightlinesensor, gpio.IN)
+gpio.setup(Leftlinesensor, gpio.IN)
+gpio.setup(in1,gpio.OUT)
+gpio.setup(in2,gpio.OUT)
+gpio.setup(in3,gpio.OUT)
+gpio.setup(in4,gpio.OUT)
+gpio.setup(enA,gpio.OUT)
+gpio.setup(enB,gpio.OUT)
+gpio.output(in1,gpio.LOW)
+gpio.output(in2,gpio.LOW)
+gpio.output(in3,gpio.LOW)
+gpio.output(in4,gpio.LOW)
+p1=gpio.PWM(enA,1000)
+p2=gpio.PWM(enB,1000)
+p1.start(30)
+p2.start(30)
+
+try:
+    while True:
+        if gpio.input((Rightlinesensor and Leftlinesensor) == False):
+            #when it sees white
+            gpio.output(in1,gpio.HIGH)
+            gpio.output(in2,gpio.LOW)
+            gpio.output(in3,gpio.HIGH)
+            gpio.output(in4,gpio.LOW)
+        else:
+            if gpio.input(RightMiddlelinesensor):
+                #Black
+                print("right forward")
+                gpio.output(in1,gpio.HIGH)
+                gpio.output(in2,gpio.LOW)
+            else:
+                #White
+                print("right backwards")
+                gpio.output(in1,gpio.LOW)
+                gpio.output(in2,gpio.HIGH)
+            if gpio.input(LeftMiddlelinesensor):
+                #Black
+                print("left forward")
+                gpio.output(in3,gpio.HIGH)
+                gpio.output(in4,gpio.LOW)
+            else:
+                #White
+                print("left backwards")
+                gpio.output(in3,gpio.LOW)
+                gpio.output(in4,gpio.HIGH)
+                
+                #right turn
+            if gpio.input(Rightlinesensor == False):
+                #when it sees white
+                gpio.output(in1,gpio.LOW)
+                gpio.output(in2,gpio.HIGH)
+                gpio.output(in3,gpio.HIGH)
+                gpio.output(in4,gpio.LOW)
+                sleep(2) #this is to time the turn
+
+                #left turn
+            if gpio.input(Leftlinesensor == False):
+                #when it sees white
+                gpio.output(in1,gpio.HIGH)
+                gpio.output(in2,gpio.LOW)
+                gpio.output(in3,gpio.LOW)
+                gpio.output(in4,gpio.HIGH)
+                sleep(2) #this is to time the turn
+
+
+finally:
+    #cleanup the gpio pins before ending
+    gpio.cleanup()

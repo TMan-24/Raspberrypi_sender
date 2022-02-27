30c29b1b002b70deefb1b8ca7b15bd0ab3dfc0b9
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-02-27 14:03:30 -0600

    updating

--- linetest4sensors.py
+++ linetest4sensors.py
@@ -36,7 +36,7 @@ p2.start(30)
 
 try:
     while True:
-        if gpio.input(RightMiddlelinesensor) == False:
+        if gpio.input(LeftMiddlelinesensor) == False:
             #when it sees white
             print("hi")
             gpio.output(in1,gpio.HIGH)

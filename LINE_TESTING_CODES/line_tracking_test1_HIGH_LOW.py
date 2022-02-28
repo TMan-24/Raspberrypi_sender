cf7cb02a82dfb21c3c7d73c57a89e33324fb0a5f
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-02-28 13:30:13 -0600

    updating

--- LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
+++ LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
@@ -283,6 +283,8 @@ def main():
                 set_motor(LEFT_MOTOR, FORWARD)
                 set_motor(RIGHT_MOTOR, FORWARD)
                 sleep(1)
+                set_motor(LEFT_MOTOR, BRAKE)
+                set_motor(LEFT_MOTOR, BRAKE)
             #2. 90deg turn - either rightmost or leftmost sensor false (off) 
             elif gpio.input(R_SENSOR) == gpio.LOW:
                 print("we are in state 2")

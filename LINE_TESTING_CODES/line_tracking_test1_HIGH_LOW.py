75f5ee7a1bc37bab0cb1f0886fdbca72beeb74ce
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-02-28 10:05:16 -0600

    updating

--- LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
+++ LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
@@ -188,7 +188,7 @@ def main():
     gpio.setup(US_BIT3, gpio.IN)
     # Starting condition will be all four sensors are off (0), since starting square is all white
     # Check to see if the robot has come off the starting square
-    while gpio.input(RM_SENSOR) == gpio.LOW and gpio.input(LM_SENSOR) == gpio.LOW and gpio.input(R_SENSOR) == gpio.LOW and gpio.input(L_SENSOR) == gpio.LOW:
+    if gpio.input(RM_SENSOR) == gpio.LOW and gpio.input(LM_SENSOR) == gpio.LOW and gpio.input(R_SENSOR) == gpio.LOW and gpio.input(L_SENSOR) == gpio.LOW:
         # Set H-Bridge to go straight
         set_motor(LEFT_MOTOR, FORWARD)
         set_motor(RIGHT_MOTOR, FORWARD)

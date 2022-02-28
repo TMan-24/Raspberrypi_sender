9d8733605f0606969d636a55755599f6fefc9736
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-02-28 13:05:22 -0600

    updating

--- LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
+++ LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
@@ -72,8 +72,8 @@ p1=gpio.PWM(EN_LM, 1000)
 p2=gpio.PWM(EN_RM, 1000)
 
     # Start motors
-p1.start(30) #motor speeds
-p2.start(30)
+p1.start(25) #motor speeds
+p2.start(25)
 
     ###### THIS WILL BE DIFFERENT PROBABLY ######
     # Set up GPIO for ultrasonic sensor as 4 bit input
@@ -282,7 +282,7 @@ def main():
                 set_motor(LEFT_MOTOR, FORWARD)
                 set_motor(RIGHT_MOTOR, FORWARD)
             #2. 90deg turn - either rightmost or leftmost sensor false (off) 
-            elif gpio.input(R_SENSOR) == gpio.LOW  or gpio.input(L_SENSOR) == gpio.LOW:
+            elif gpio.input(R_SENSOR) == gpio.LOW and gpio.input(RM_SENSOR) == gpio.LOW:
                 print("we are in state 2")
                 turn_90(gpio.input(R_SENSOR))
             #3. correct back to line - use two middle sensors to determine

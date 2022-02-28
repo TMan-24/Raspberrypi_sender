f3d2671090491422a132b740d9b80acc5672e5c1
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-02-27 18:34:24 -0600

    updating

--- LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
+++ LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
@@ -96,48 +96,6 @@ def set_motor(motor_num, state):
 
     else:
         print("Invalid motor paramter")
-
-
-
-
-# Set pinout mode to Broadcom (board communication)
-gpio.setmode(gpio.BCM)
-
-    # Line sensor setup as input digital pins
-gpio.setup(RM_SENSOR, gpio.IN)
-gpio.setup(LM_SENSOR, gpio.IN)
-gpio.setup(R_SENSOR, gpio.IN)
-gpio.setup(L_SENSOR, gpio.IN)
-
-    # Setup H-bridge inputs as output pins
-gpio.setup(RM_FORWARD, gpio.OUT)
-gpio.setup(RM_BACKWARD, gpio.OUT)
-gpio.setup(LM_FORWARD, gpio.OUT)
-gpio.setup(LM_BACKWARD, gpio.OUT)
-
-    # Set H-bridge Enable motor signals as output pins 
-gpio.setup(EN_LM, gpio.OUT)
-gpio.setup(EN_RM, gpio.OUT)
-
-    # initialize motor to go forward
-set_motor(LEFT_MOTOR, FORWARD)
-set_motor(RIGHT_MOTOR, FORWARD)
-
-# Set a PWM signal of 1000 for both motors
-p1=gpio.PWM(EN_LM, 1000)
-p2=gpio.PWM(EN_RM, 1000)
-
-    # Start motors
-p1.start(30) #motor speeds
-p2.start(30)
-
-    ###### THIS WILL BE DIFFERENT PROBABLY ######
-    # Set up GPIO for ultrasonic sensor as 4 bit input
-    # This will require multiple GPIO pins 
-gpio.setup(US_BIT0, gpio.IN)
-gpio.setup(US_BIT1, gpio.IN)
-gpio.setup(US_BIT2, gpio.IN)
-gpio.setup(US_BIT3, gpio.IN)
     
 
 '''def read_ultrasound():
@@ -184,7 +142,44 @@ def main():
     
     #begin initialization of the GPIO pins on the pi
     #init_gpio()
+    # Set pinout mode to Broadcom (board communication)
+    gpio.setmode(gpio.BCM)
 
+    # Line sensor setup as input digital pins
+    gpio.setup(RM_SENSOR, gpio.IN)
+    gpio.setup(LM_SENSOR, gpio.IN)
+    gpio.setup(R_SENSOR, gpio.IN)
+    gpio.setup(L_SENSOR, gpio.IN)
+
+    # Setup H-bridge inputs as output pins
+    gpio.setup(RM_FORWARD, gpio.OUT)
+    gpio.setup(RM_BACKWARD, gpio.OUT)
+    gpio.setup(LM_FORWARD, gpio.OUT)
+    gpio.setup(LM_BACKWARD, gpio.OUT)
+
+    # Set H-bridge Enable motor signals as output pins 
+    gpio.setup(EN_LM, gpio.OUT)
+    gpio.setup(EN_RM, gpio.OUT)
+
+    # initialize motor to go forward
+    set_motor(LEFT_MOTOR, FORWARD)
+    set_motor(RIGHT_MOTOR, FORWARD)
+
+# Set a PWM signal of 1000 for both motors
+    p1=gpio.PWM(EN_LM, 1000)
+    p2=gpio.PWM(EN_RM, 1000)
+
+    # Start motors
+    p1.start(30) #motor speeds
+    p2.start(30)
+
+    ###### THIS WILL BE DIFFERENT PROBABLY ######
+    # Set up GPIO for ultrasonic sensor as 4 bit input
+    # This will require multiple GPIO pins 
+    gpio.setup(US_BIT0, gpio.IN)
+    gpio.setup(US_BIT1, gpio.IN)
+    gpio.setup(US_BIT2, gpio.IN)
+    gpio.setup(US_BIT3, gpio.IN)
     # Starting condition will be all four sensors are off (0), since starting square is all white
     # Check to see if the robot has come off the starting square
     while gpio.input(RM_SENSOR) == gpio.LOW and gpio.input(LM_SENSOR) == gpio.LOW and gpio.input(R_SENSOR) == gpio.LOW and gpio.input(L_SENSOR) == gpio.LOW:
@@ -195,7 +190,7 @@ def main():
     # main logic of program
     while TRUE:
         #1. continue straight - innermost sensors are on and outer sensors are not on
-        if gpio.input(R_SENSOR) == gpio.HIGH and gpio.input(L_SENSOR) == gpio.HIGH and gpio.input(RM_SENSOR) == gpio.LOW and gpio.input(LM_SENSOR) == gpio.LOW:
+        if gpio.input(R_SENSOR) == gpio.HIGH and gpio.input(L_SENSOR) == gpio.HIGH and gpio.input(RM_SENSOR) == gpio.HIGH and gpio.input(LM_SENSOR) == gpio.HIGH:
             # Set H-Bridge to go straight
             set_motor(LEFT_MOTOR, FORWARD)
             set_motor(RIGHT_MOTOR, FORWARD)
@@ -209,10 +204,10 @@ def main():
         #3. correct back to line - use two middle sensors to determine
         elif gpio.input(RM_SENSOR) == gpio.LOW:
             set_motor(LEFT_MOTOR, FORWARD)
-            set_motor(RIGHT_MOTOR, BRAKE)
+            set_motor(RIGHT_MOTOR, BACKWARD)
             print("we are in state 3.1")
         else:
-            set_motor(LEFT_MOTOR, BRAKE)
+            set_motor(LEFT_MOTOR, BACKWARD)
             set_motor(RIGHT_MOTOR, FORWARD)
             print("we are in state 3")
 

d8fa83e29603b24f4b6a521acc67c24abb16472f
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-02-28 11:06:19 -0600

    updating

--- LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
+++ LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
@@ -251,6 +251,7 @@ def main():
         else:
             print("Right: 1")
     
+    sleep(5)
     # main logic of program
     while TRUE:
         if gpio.input(L_SENSOR) == gpio.LOW:
@@ -275,9 +276,9 @@ def main():
             print("we are in state 1")
             set_motor(LEFT_MOTOR, FORWARD)
             set_motor(RIGHT_MOTOR, FORWARD)
-
+        
         #2. 90deg turn - either rightmost or leftmost sensor false (off) 
-        elif (gpio.input(R_SENSOR) == gpio.LOW and gpio.input(RM_SENSOR) == gpio.LOW) or (gpio.input(L_SENSOR) == gpio.LOW and gpio.input(LM_SENSOR) == gpio.LOW and gpio.input(RM_SENSOR)):
+        elif gpio.input(R_SENSOR) == gpio.LOW  or gpio.input(L_SENSOR) == gpio.LOW:
             print("we are in state 2")
             turn_90(gpio.input(R_SENSOR))
 

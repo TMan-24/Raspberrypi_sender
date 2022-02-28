c7dc530a62ca88c852bfa76cddf19bff3420dcf1
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-02-28 11:26:07 -0600

    updating

--- LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
+++ LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
@@ -251,7 +251,7 @@ def main():
             print("Right: 0") 
         else:
             print("Right: 1")
-    sleep(2)
+        sleep(2)
     if gpio.input(L_SENSOR) == gpio.LOW and gpio.input(LM_SENSOR) == gpio.LOW and gpio.input(RM_SENSOR) == gpio.LOW and gpio.input(R_SENSOR):
         main()
     else:

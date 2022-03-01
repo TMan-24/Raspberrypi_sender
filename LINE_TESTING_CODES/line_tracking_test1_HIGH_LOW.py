1b06a625f706ee0d57627f85d86822f0191390d6
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-03-01 09:54:45 -0600

    updating

--- LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
+++ LINE_TESTING_CODES/line_tracking_test1_HIGH_LOW.py
@@ -232,9 +232,8 @@ def main():
                 set_motor(LEFT_MOTOR, FORWARD)
             #2. 90deg turn - either rightmost or leftmost sensor false (off) 
             elif gpio.input(R_SENSOR) == gpio.LOW:
-                set_motor(RIGHT_MOTOR, BACKWARD)
-                set_motor(LEFT_MOTOR, BRAKE)
-                sleep(0.5)
+                #set_motor(RIGHT_MOTOR, BACKWARD)
+                #sleep(0.5)
                 turn_90(gpio.input(R_SENSOR))
             #3. correct back to line - use two middle sensors to determine
             elif gpio.input(RM_SENSOR) == gpio.LOW:

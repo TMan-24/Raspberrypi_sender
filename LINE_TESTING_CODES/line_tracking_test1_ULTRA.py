0e6a1827fb355733b9c84dbf8b137d9f9ccc4ca8
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-03-04 11:02:32 -0600

    updating

--- LINE_TESTING_CODES/line_tracking_test1_ULTRA.py
+++ LINE_TESTING_CODES/line_tracking_test1_ULTRA.py
@@ -293,10 +293,10 @@ def main():
                 set_motor(RIGHT_MOTOR, FORWARD)
                 
             #4. 180deg turn (turn around) - additional logic needed to avoid 180deg turn at first 90deg turn 
-                dist = read_ultrasound()
-                print ("Measured Distance", dist)
-                if dist == THRESHOLD_VALUE:
-                    print ("Measured Distance")
+            dist = read_ultrasound()
+            print ("Measured Distance", dist)
+            if dist == THRESHOLD_VALUE:
+                print ("Measured Distance")
                 #print("value from sensor: ", read_ultrasound())
                 #print("value from sensor: ", THRESHOLD_VALUE)
                 #turn_around()

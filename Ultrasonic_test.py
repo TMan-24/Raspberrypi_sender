d6f2f8c14eb8bf578f9cbe1092e013f7f76d81d6
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-03-04 11:36:00 -0600

    updating

--- Ultrasonic_test.py
+++ Ultrasonic_test.py
@@ -76,8 +76,8 @@ def distance2():
 if __name__ == '__main__':
     try:
         while True:
-            #dist1 = distance1()
-            #print ("Measured Distance1 = %.1f cm" % dist1)
+            dist1 = distance1()
+            print ("Measured Distance1 = %.1f cm" % dist1)
             dist2 = distance2()
             print ("Measured Distance2 = %.1f cm" % dist2)
             time.sleep(1)

efd18931cb03794003a11d37d78b281ff6dacda0
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-03-04 12:56:17 -0600

    updating

--- LINE_TESTING_CODES/line_tracking_test1_ULTRA.py
+++ LINE_TESTING_CODES/line_tracking_test1_ULTRA.py
@@ -323,10 +323,10 @@ def main():
                 set_motor(RIGHT_MOTOR, FORWARD)
                 
             #4. 180deg turn (turn around) - additional logic needed to avoid 180deg turn at first 90deg turn 
-            #dist1 = read_ultrasound()
-            #dist2 = read_ultrasound2()
-            #if (dist1 and dist2) <= THRESHOLD_VALUE:
-             #   turn_around()
+            dist1 = read_ultrasound()
+            dist2 = read_ultrasound2()
+            if (dist1 and dist2) <= THRESHOLD_VALUE:
+                turn_around()
 
             #5. if we get back to starting position, stop program
             #if gpio.input(RM_SENSOR) == gpio.LOW and gpio.input(LM_SENSOR) == gpio.LOW and gpio.input(R_SENSOR) == gpio.LOW and gpio.input(L_SENSOR) == gpio.LOW:

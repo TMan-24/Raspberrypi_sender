19c2c55c6ee62afa7ae67a938afa1b4a064a6d9e
Tman_25 <tmanscomputer@tonys-air.utm.edu>
2022-03-17 13:06:23 -0500

    updating

--- LINE_TESTING_CODES/Auto_Loop_EDITS.py
+++ LINE_TESTING_CODES/Auto_Loop_EDITS.py
@@ -309,7 +309,7 @@ def main():
                 print("180 Turn Around")
                 set_motor(LEFT_MOTOR, BACKWARD)
                 set_motor(RIGHT_MOTOR, BACKWARD)
-                sleep(0.5)
+                sleep(1)
                 turn_around()
                 print("Robot is now Driving Straight")
             

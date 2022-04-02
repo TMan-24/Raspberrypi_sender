177c0b1da2a7b949fa76d6c22393c5fd9b90dff7
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-04-02 09:22:22 -0500

    updating

--- Auto_Loop_EDITS.py
+++ Auto_Loop_EDITS.py
@@ -85,40 +85,40 @@ END_PROGRAM_VALUE2 = 20 # Distance in cm for ending wall (correcting for hardwar
 END_PROGRAM_VALUE3 = 21 # Error variable
 
 # Set warnings to false
-    gpio.setwarnings(False)
-    
-    # Set pinout mode to Broadcom (board communication)
-    gpio.setmode(gpio.BCM)
+gpio.setwarnings(False)
 
-    # Line sensor setup as input digital pins
-    gpio.setup(RM_SENSOR, gpio.IN)
-    gpio.setup(LM_SENSOR, gpio.IN)
-    gpio.setup(R_SENSOR, gpio.IN)
-    gpio.setup(L_SENSOR, gpio.IN)
+# Set pinout mode to Broadcom (board communication)
+gpio.setmode(gpio.BCM)
 
-    # Setup H-bridge inputs as output pins
-    gpio.setup(RM_FORWARD, gpio.OUT)
-    gpio.setup(RM_BACKWARD, gpio.OUT)
-    gpio.setup(LM_FORWARD, gpio.OUT)
-    gpio.setup(LM_BACKWARD, gpio.OUT)
+# Line sensor setup as input digital pins
+gpio.setup(RM_SENSOR, gpio.IN)
+gpio.setup(LM_SENSOR, gpio.IN)
+gpio.setup(R_SENSOR, gpio.IN)
+gpio.setup(L_SENSOR, gpio.IN)
 
-    # Set H-bridge Enable motor signals as output pins 
-    gpio.setup(EN_LM, gpio.OUT)
-    gpio.setup(EN_RM, gpio.OUT)
+# Setup H-bridge inputs as output pins
+gpio.setup(RM_FORWARD, gpio.OUT)
+gpio.setup(RM_BACKWARD, gpio.OUT)
+gpio.setup(LM_FORWARD, gpio.OUT)
+gpio.setup(LM_BACKWARD, gpio.OUT)
 
-    # Set a PWM signal of 1000 for both motors
-    p1=gpio.PWM(EN_LM, 1000)
-    p2=gpio.PWM(EN_RM, 1000)
+# Set H-bridge Enable motor signals as output pins 
+gpio.setup(EN_LM, gpio.OUT)
+gpio.setup(EN_RM, gpio.OUT)
 
-    # Start motors
-    p1.start(30) #motor speeds
-    p2.start(30)
+# Set a PWM signal of 1000 for both motors
+p1=gpio.PWM(EN_LM, 1000)
+p2=gpio.PWM(EN_RM, 1000)
 
-    # Set up GPIO for ultrasonic sensor
-    gpio.setup(TRIGGER1, gpio.OUT)
-    gpio.setup(TRIGGER2, gpio.OUT)
-    gpio.setup(ECHO1, gpio.IN)
-    gpio.setup(ECHO2, gpio.IN)
+# Start motors
+p1.start(30) #motor speeds
+p2.start(30)
+
+# Set up GPIO for ultrasonic sensor
+gpio.setup(TRIGGER1, gpio.OUT)
+gpio.setup(TRIGGER2, gpio.OUT)
+gpio.setup(ECHO1, gpio.IN)
+gpio.setup(ECHO2, gpio.IN)
 
 # Set_motor function - sets motor to forward/backward/brake
 def set_motor(motor_num, state):

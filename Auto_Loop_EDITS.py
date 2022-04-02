baaf1da0a5257dcc1ded9eecc332bacfff4752fe
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-04-02 10:29:01 -0500

    updating

--- Auto_Loop_EDITS.py
+++ Auto_Loop_EDITS.py
@@ -255,7 +255,7 @@ def read_ultrasound2():
 def turn_around():
     set_motor(LEFT_MOTOR, FORWARD)
     set_motor(RIGHT_MOTOR, BACKWARD)
-    sleep(6)
+    sleep(4)
     #while gpio.input(L_SENSOR) == gpio.HIGH and gpio.input(LM_SENSOR) == gpio.HIGH:
      #   set_motor(LEFT_MOTOR, FORWARD)
       #  set_motor(RIGHT_MOTOR, BACKWARD)

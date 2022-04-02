2e63368ac2ab0c9399fd360730f8afcafa1b4205
Tman_25 <tmanscomputer@Tonys-MacBook-Air.local>
2022-04-02 10:27:00 -0500

    updating

--- Auto_Loop_EDITS.py
+++ Auto_Loop_EDITS.py
@@ -331,7 +331,7 @@ def main():
                     sleep(2)
                     set_motor(LEFT_MOTOR, BACKWARD)
                     set_motor(RIGHT_MOTOR, BACKWARD)
-                    sleep(0.3)
+                    sleep(0.5)
                     turn_around()
                     print("Robot is now Driving Straight")
                 if Turn == False and Turn2 == False:
